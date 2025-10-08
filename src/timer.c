2cdf53b617f1b39ee9ead436417b7893e6ee4076
Sem <semdejong.elektro@gmail.com>
2025-10-08 14:59:14 +0200

    yes

--- src/timer.c
+++ src/timer.c
@@ -109,16 +109,16 @@ bool timer_get_pulse_done(){
  */
 void TIMER0_IRQHandler (void)
 {
+  TIMER
     if(pulse_pwm)
     {
         /* Clear the interrupt flag in the beginning */
         //pulseCount >= periodsPerPulse*2 ||
        // pulseCount++;
-        TIMER_Enable(TIMER0, false);
         if (pulseCount >= periodsPerPulse)
         {
 
-       //     TIMER_Enable(TIMER0, false);
+            TIMER_Enable(TIMER0, false);
             pulse_done = true;
             pulseCount = 0;
 
@@ -129,7 +129,6 @@ void TIMER0_IRQHandler (void)
         }
         else
         {
-            TIMER_Enable(TIMER0, true);
             pulseCount++; //16 cycles
         }
         uint32_t flags = TIMER_IntGet(TIMER0);
